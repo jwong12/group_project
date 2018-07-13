74536c018947ac1a7a3b307be3972845a44f7d1c
James Wong <jwong_li@hotmail.com>
2018-07-13 15:55:47 -0700

    nav and links fixed

---
+++ css/header.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('.navbar-toggler').click(function(){
+        $('#navbarSupportedContent').slideToggle();
+    });
+
+    var viewport = $(window).width();
+    console.log(viewport);
+    if(viewport >= 974.5){
+        $('#dropdown1').hover(function(){
+            $('#menu-dropdown1').slideToggle();
+            viewport = $('body').width();
+        });
+    } else {
+        
+        $('#dropdown1').click(function(){
+            $('#menu-dropdown1').slideToggle();
+            viewport = $('body').width();
+        });
+    }
+
+
+    if(viewport >= 974.5){
+        $('#more-dropdown2').hover(function(){
+            $('#menu-dropdown2').slideToggle();
+            viewport = $('body').width();
+        });
+    } else {
+        
+        $('#more-dropdown2').click(function(){
+            $('#menu-dropdown2').slideToggle();
+            viewport = $('body').width();
+        });
+    }
+
+
+    
+});
--- js/lecture03_script.js
+++ js/lecture03_script.js
@@ -3,7 +3,41 @@
     Date: July 10th, 2018
 */
 $(document).ready(function(){
+    /* Navigation dropdown script */
+    $('.navbar-toggler').click(function(){
+        $('#navbarSupportedContent').slideToggle();
+    });
 
+    var viewport = $(window).width();
+    console.log(viewport);
+    if(viewport >= 974.5){
+        $('#dropdown1').hover(function(){
+            $('#menu-dropdown1').slideToggle();
+            viewport = $('body').width();
+        });
+    } else {
+        
+        $('#dropdown1').click(function(){
+            $('#menu-dropdown1').slideToggle();
+            viewport = $('body').width();
+        });
+    }
+    
+    if(viewport >= 974.5){
+        $('#more-dropdown2').hover(function(){
+            $('#menu-dropdown2').slideToggle();
+            viewport = $('body').width();
+        });
+    } else {
+        
+        $('#more-dropdown2').click(function(){
+            $('#menu-dropdown2').slideToggle();
+            viewport = $('body').width();
+        });
+    }
+
+
+    /* Heading toggle and bookmark script */
     $(".lecture_content").hide();
     
     $(".box1").click(function (event){
